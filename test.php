3c3b6d62aef13656d2c505e7ec935ca9484fb61b
Ying Huang <yhuang@imvu.com>
2017-07-26 15:05:26 -0700

    Add test php file.

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+
+class service {
+    public function multiget($product_ids) {
+        if (empty($product_ids)) {
+            return [];
+        }
+
+        foreach ($urls as $url) {
+            if (null === $url) {
+                continue;
+            }
+            $argLists[] = [$url->to_string()];
+        }
+
+        foreach ($urls as $url) {
+            if (null === $url) {
+                $result[] = null;
+            } else {
+                $result[] = \webcore\arrays::each_value($values);
+            }
+        }
+
+        return $result;
+    }
+}
